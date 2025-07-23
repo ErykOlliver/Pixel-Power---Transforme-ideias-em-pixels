dbbb1449aef3aa23227500b0b08678a9d0832781
Eryk Olliver <erykolliver@gmail.com>
2025-07-23 16:12:08 -0300

    Modulos adicionados com sucesso! Iniciando Seção: Será que esse curso é pra mim?

--- src/assets/components/Others/Pop/BaseModules.jsx
+++ src/assets/components/Others/Pop/BaseModules.jsx
@@ -1,9 +1,9 @@
 export default function BaseModule({ Icon, Text }) {
     return (
-        <div className=" w-72 h-96 border flex justify-center items-center pl-2.5 pr-2.5 pb-2.5 pt-2.5">
-            <div className="h-full w-[90%] border flex flex-col items-center justify-center ">
+        <div className=" min-w-72 max-w-80 h-96 shadow-[0_5px_5px_-6px] bg-gradient-to-b from-Sunset-Blaze to-Golden-Ember rounded-xl flex justify-center items-center pl-2.5 pr-2.5 pb-2.5 pt-2.5">
+            <div className="h-full w-[90%] flex flex-col items-center justify-center gap-3">
                 {Icon}
-                <p className="text-2xl text-center">{Text}</p>
+                <p className="text-xl text-center w-full text-White-90 font-bold font-Inria">{Text}</p>
             </div>
         </div>
     )
--- src/assets/screens/Modules.jsx
+++ src/assets/screens/Modules.jsx
@@ -1,4 +1,4 @@
-import { Grid02Icon, GridIcon } from '@hugeicons/core-free-icons'
+import { BrushIcon, FlyingHumanIcon, Grid02Icon, GridIcon, KeyframeAddIcon, MapsSearchIcon, PaintBoardIcon, ParagraphBulletsPoint01FreeIcons, RubiksCubeIcon, ToolsIcon } from '@hugeicons/core-free-icons'
 import BaseModule from '../components/Others/Pop/BaseModules.jsx'
 import { HugeiconsIcon } from '@hugeicons/react'
 import { Power_Colors } from '../components/Design/Pallet.js'
@@ -7,11 +7,41 @@ export default function Modules() {
     return (
         <section className='flex justify-center h-screen w-screen items-center bg-White-80'>
             <div className='flex flex-col items-center justify-center h-full w-[90%] gap-3.5 '>
-                <h1>o que você vai aprender no curso:</h1>
-                <BaseModule
-                    Text='Aprenda os fundamentos da pixel art: resolução, paleta, grid e leitura visual. A base para qualquer estilo.'
-                    Icon={<HugeiconsIcon icon={GridIcon} size={125} color={Power_Colors.White90} />}
-                />
+                <h1 className='text-5xl font-Inter font-bold'>o que você vai aprender no curso:</h1>
+                <div className="flex flex-wrap gap-9 justify-center w-full h-full items-center">
+                    <BaseModule
+                        Text="Aprenda os fundamentos da pixel art: resolução, paleta, grid e leitura visual. A base para qualquer estilo."
+                        Icon={<HugeiconsIcon icon={GridIcon} size={125} color={Power_Colors.White90} />}
+                    />
+                    <BaseModule
+                        Text="Construa formas legíveis e impactantes. Aprenda a criar personagens e objetos com presença visual."
+                        Icon={<HugeiconsIcon icon={RubiksCubeIcon} size={125} color={Power_Colors.White90} />}
+                    />
+                    <BaseModule
+                        Text="Entenda como cores e luz criam profundidade, contraste e estilo em pixel art com poucos tons."
+                        Icon={<HugeiconsIcon icon={PaintBoardIcon} size={125} color={Power_Colors.White90} />}
+                    />
+                    <BaseModule
+                        Text="Crie personagens com expressão e identidade mesmo com poucos pixels. Poses, anatomia e estilo."
+                        Icon={<HugeiconsIcon icon={FlyingHumanIcon} size={125} color={Power_Colors.White90} />}
+                    />
+                    <BaseModule
+                        Text="Desenvolva cenários e tiles com coesão. Trabalhe repetição, perspectiva e ambientação visual."
+                        Icon={<HugeiconsIcon icon={MapsSearchIcon} size={125} color={Power_Colors.White90} />}
+                    />
+                    <BaseModule
+                        Text="Desenhe HUDs, menus, ícones e objetos com clareza visual e linguagem funcional para jogos."
+                        Icon={<HugeiconsIcon icon={ToolsIcon} size={125} color={Power_Colors.White90} />}
+                    />
+                    <BaseModule
+                        Text="Aprenda a dar vida aos pixels com animações simples e impactantes: movimentos, loops e efeitos."
+                        Icon={<HugeiconsIcon icon={KeyframeAddIcon} size={125} color={Power_Colors.White90} />}
+                    />
+                    <BaseModule
+                        Text="Explore seu estilo e monte um portfólio pronto para o mercado, jogos, ou projetos autorais."
+                        Icon={<HugeiconsIcon icon={BrushIcon} size={125} color={Power_Colors.White90} />}
+                    />
+                </div>
             </div>
         </section>
     )
