ed44a910b50de3cb805792327b9fa3ce21f94cdd
Eryk Olliver <erykolliver@gmail.com>
2025-07-23 16:16:14 -0300

    Seção CurseForMe limpa para começar a construção

---
+++ src/assets/screens/CurseForMe.jsx
@@ -0,0 +1,20 @@
+import { BrushIcon, FlyingHumanIcon, Grid02Icon, GridIcon, KeyframeAddIcon, MapsSearchIcon, PaintBoardIcon, ParagraphBulletsPoint01FreeIcons, RubiksCubeIcon, ToolsIcon } from '@hugeicons/core-free-icons'
+import { HugeiconsIcon } from '@hugeicons/react'
+import { Power_Colors } from '../components/Design/Pallet.js'
+
+export default function CurseForMe() {
+    return (
+        <section className='flex justify-center h-screen w-screen items-center bg-White-80'>
+            <div className='flex flex-col items-center justify-center h-full w-[90%] gap-3.5 '>
+                <h1 className='text-5xl font-Inter font-bold'>“Sera que esse curso é para mim?”</h1>
+                <p>
+                    Se você quer aprender pixel art com técnica, propósito e liberdade criativa, esse curso foi feito para você.
+                    Não importa seu nível — aqui você encontra direção e prática para evoluir do seu jeito.
+                </p>
+                <div className="flex flex-wrap gap-9 justify-center w-full h-full items-center">
+
+                </div>
+            </div>
+        </section>
+    )
+}
