dcbbc2a990279f890099c66b5bb9b23cdff7f2ef
Eryk Olliver <erykolliver@gmail.com>
2025-07-23 18:17:29 -0300

    GenericPage finalizada

---
+++ src/assets/screens/GenericPage.jsx
@@ -0,0 +1,11 @@
+import { BrushIcon, FlyingHumanIcon, Grid02Icon, GridIcon, KeyframeAddIcon, MapsSearchIcon, PaintBoardIcon, ParagraphBulletsPoint01FreeIcons, RubiksCubeIcon, ToolsIcon } from '@hugeicons/core-free-icons'
+
+export default function GenericPage() {
+    return (
+        <section className='flex justify-center h-screen w-screen items-center bg-White-80'>
+            <div className='flex items-center justify-center h-full w-[90%] gap-3.5 '>
+                <h1 className='text-5xl font-Inter font-bold'>ainda não está convencido?</h1>
+            </div>
+        </section>
+    )
+}
