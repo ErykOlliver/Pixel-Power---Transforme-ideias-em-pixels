f7c202a988eca993269963f5596dedeb2e69bb56
Eryk Olliver <erykolliver@gmail.com>
2025-07-24 00:17:34 -0300

    Pagina Faq preparada para receber os BaseFaq

--- src/assets/screens/Faq.jsx
+++ src/assets/screens/Faq.jsx
@@ -2,8 +2,11 @@
 export default function Faq() {
     return (
         <section className='flex justify-center h-screen w-screen items-center bg-White-80'>
-            <div className='flex items-center py-24 justify-between h-full w-[90%] gap-2.5 '>
+            <div className='flex flex-col items-center py-2.5 justify-center h-full w-[90%] gap-2.5 '>
+                <h1 className="text-5xl font-bold font-Inter">Perguntas frequentes</h1>
+                <div className="w-full h-fit border">
 
+                </div>
             </div>
         </section>
 
