50c5023070bd5baeae5dcee55a3a58f977d65a82
Eryk Olliver <erykolliver@gmail.com>
2025-07-24 00:59:16 -0300

    BaseFaq finalizado

--- src/assets/components/Others/Base/BaseFaq.jsx
+++ src/assets/components/Others/Base/BaseFaq.jsx
@@ -1,14 +1,22 @@
 import { ArrowDown01Icon, ArrowRight01Icon } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
+import { useRef, useState } from "react";
 
 export default function BaseFaq({ question, resp }) {
+    const [IsOpen, SetOpen] = useState(false)
+    const contentRef = useRef(null)
+
+    const toggle = () => SetOpen(!IsOpen)
     return (
-        <div className="border-b-2 w-full">
-            <div className="flex mx-3.5 justify-between items-center  w-full">
+        <div className=" w-full ">
+            <div onClick={toggle} className="border-b-2 flex hover:cursor-pointer justify-between items-center  w-full">
                 <h2>{question}</h2>
-                <div className="w-fit h-fit rotate-0">
+                <div className={`w-fit h-fit transition-all duration-300 ${IsOpen ? 'rotate-90' : 'rotate-0'}`}>
                     <HugeiconsIcon icon={ArrowRight01Icon} size={70} />
                 </div>
+            </div>
+            <div ref={contentRef} className={`transition-all duration-300 overflow-hidden ${IsOpen ? 'max-h-[1000px]' : 'max-h-0'}`}>
+                <p className="">{resp}</p>
             </div>
         </div>
     )
--- src/assets/screens/Faq.jsx
+++ src/assets/screens/Faq.jsx
@@ -5,9 +5,18 @@ export default function Faq() {
         <section className='flex justify-center h-screen w-screen items-center bg-White-80'>
             <div className='flex flex-col items-center py-2.5 justify-center h-full w-[90%] gap-2.5 '>
                 <h1 className="text-5xl font-bold font-Inter">Perguntas frequentes</h1>
-                <div className="w-full h-fit ">
+                <div className="w-full h-fit flex flex-col gap-3.5 ">
                     <BaseFaq
                         question="O curso é mesmo para iniciantes?"
+                        resp="Ola Mundo"
+                    />
+                    <BaseFaq
+                        question="O curso é mesmo para iniciantes?"
+                        resp="Ola Mundo"
+                    />
+                    <BaseFaq
+                        question="O curso é mesmo para iniciantes?"
+                        resp="Ola Mundo"
                     />
                 </div>
             </div>
